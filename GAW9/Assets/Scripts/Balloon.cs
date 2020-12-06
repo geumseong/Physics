21d6c0eb9b4bdfcc3e7155e7842647df9466de5b
Geumseong Han <geumseong.han@ue-germany.de>
2020-12-06 23:52:16 +0900

    - Added Bird Movement (Bird.cs)  - Added Balloon player controls (Balloon going up & down) (Balloon.cs)  - Added Scrolling Background (BgScroll.cs)

---
+++ GAW9/Assets/Scripts/Balloon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float riseForce = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space)) {
+            Rise();
+        }
+    }
+
+    /*
+    void FixedUpdate() {
+        if(Input.GetKey("space")) {
+            Rise();
+        }
+    }
+    */
+
+    void Rise() {
+        rb.AddForce(Vector2.up * riseForce);
+    }
+}
---
+++ GAW9/Assets/Scripts/Bird.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        Vector2 direction = new Vector2(1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition((Vector2)transform.position + (Vector2.left * Time.deltaTime * speed));
+    }
+}
